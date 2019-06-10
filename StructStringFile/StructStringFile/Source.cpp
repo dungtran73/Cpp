cc7673926561cb33739f90a5a6169b23551905c9
Dung Tran <trandung050797@gmail.com>
2019-06-10 14:04:36 +0700

    Hien thi MENU

--- StructStringFile/StructStringFile/Source.cpp
+++ StructStringFile/StructStringFile/Source.cpp
@@ -4,9 +4,62 @@ using namespace std;
 
 struct Student
 {
-
+	int id;
+	string name;
+	float score;
 };
+void Input();
+void Display();
+void Save();
+void Load();
+void Exit();
 void main()
 {
+	cout << "------------------MENU-------------------" << endl;
+	cout << "\t1. Input" << endl;
+	cout << "\t2. Display" << endl;
+	cout << "\t3. Save to file" << endl;
+	cout << "\t4. Load from file" << endl;
+	cout << "\t0. Exit" << endl;
+	cout << "-----------------------------------------" << endl;
+	cout << "Choose: ";
+	int answer;
+	cin >> answer;
+	switch (answer)
+	{
+	case 1:
+		Input();
+		break;
+	case 2:
+		Display();
+		break;
+	case 3:
+		Save();
+		break;
+	case 4:
+		Load();
+		break;
+	default:
+		break;
+	}
+}
 
+void Input()
+{
+}
+
+void Display()
+{
+}
+
+void Save()
+{
+}
+
+void Load()
+{
+}
+
+void Exit()
+{
 }
